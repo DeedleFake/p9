d0cd56bd45847d9d54b4d1647b1efd4988d1b331
Yissakhar Z. Beck <yissakhar@moquality.com>
2018-04-27 15:47:25 -0400

    Framework for sending messages.

---
+++ msg.go
@@ -0,0 +1,458 @@
+package p9
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math"
+)
+
+// A Message is any 9P message, either T or R, minus the tag. Tags are
+// handled automatically by the server.
+type Message interface {
+	// Type returns the message type.
+	Type() MessageType
+
+	// Size returns the number of bytes that the message will write in
+	// wire format.
+	Size() uint32
+
+	// WriteTo writes the message to w in wire format.
+	WriteTo(w io.Writer) (int64, error)
+}
+
+const (
+	NoTag uint16 = math.MaxUint16
+	NoFID uint32 = math.MaxUint32
+)
+
+type MessageType uint8
+
+const (
+	TversionType MessageType = 100 + iota
+	RversionType
+	TauthType
+	RauthType
+	TattachType
+	RattachType
+	terrorType // Not used.
+	RerrorType
+	TflushType
+	RflushType
+	TwalkType
+	RwalkType
+	TopenType
+	RopenType
+	TcreateType
+	RcreateType
+	TreadType
+	RreadType
+	TwriteType
+	RwriteType
+	TclunkType
+	RclunkType
+	TremoveType
+	RremoveType
+	TstatType
+	RstatType
+	TwstatType
+	RwstatType
+)
+
+func (t MessageType) encode(e *encoder) {
+	e.Encode(uint8(t))
+}
+
+type encoder struct {
+	w   io.Writer
+	n   int64
+	err error
+}
+
+func (e *encoder) Write(data []byte) (int, error) {
+	if e.err != nil {
+		return 0, e.err
+	}
+
+	n, err := e.w.Write(data)
+	e.n += int64(n)
+	e.err = err
+	return n, err
+}
+
+func (e *encoder) Encode(v interface{}) error {
+	if e.err != nil {
+		return e.err
+	}
+
+	if v, ok := v.(encodable); ok {
+		v.encode(e)
+		return e.err
+	}
+
+	switch v := v.(type) {
+	case uint8, uint16, uint32, uint64, int8, int16, int32, int64:
+		e.err = binary.Write(e, binary.LittleEndian, v)
+		return e.err
+
+	case []byte:
+		err := binary.Write(e, binary.LittleEndian, uint32(len(v)))
+		if err != nil {
+			e.err = err
+			return err
+		}
+
+		e.err = binary.Write(e, binary.LittleEndian, v)
+		return e.err
+
+	case string:
+		err := binary.Write(e, binary.LittleEndian, uint16(len(v)))
+		if err != nil {
+			e.err = err
+			return err
+		}
+
+		e.err = binary.Write(e, binary.LittleEndian, []byte(v))
+		return e.err
+
+	case []string:
+		err := binary.Write(e, binary.LittleEndian, uint16(len(v)))
+		if err != nil {
+			e.err = err
+			return err
+		}
+
+		for _, str := range v {
+			err := e.Encode(str)
+			if err != nil {
+				e.err = err
+				return err
+			}
+		}
+
+		return e.err
+
+	case []QID:
+		err := binary.Write(e, binary.LittleEndian, uint16(len(v)))
+		if err != nil {
+			e.err = err
+			return err
+		}
+
+		for _, qid := range v {
+			err := e.Encode(qid)
+			if err != nil {
+				e.err = err
+				return err
+			}
+		}
+
+		return e.err
+
+	default:
+		panic(fmt.Errorf("Unexpected type: %T", v))
+	}
+}
+
+type encodable interface {
+	encode(e *encoder)
+}
+
+type QID struct {
+	Type    QIDType
+	Version uint32
+	Path    uint64
+}
+
+func (qid QID) encode(e *encoder) {
+	e.Encode(qid.Type)
+	e.Encode(qid.Version)
+	e.Encode(qid.Path)
+}
+
+type QIDType uint8
+
+const (
+	QTFile    QIDType = 0
+	QTSymlink QIDType = 1 << iota
+	QTTmp
+	QTAuth
+	QTMount
+	QTExcl
+	QTAppend
+	QTDir
+)
+
+func (t QIDType) encode(e *encoder) {
+	e.Encode(uint8(t))
+}
+
+type Stat struct {
+	Type    uint16
+	Dev     uint32
+	QIDType QIDType
+	Version uint32
+	Path    uint64
+	Mode    uint32 // TODO: Make a Mode type?
+	ATime   uint32
+	MTime   uint32
+	Length  uint64
+	Name    string
+	UID     string
+	GID     string
+	MUID    string
+}
+
+func (s *Stat) encode(e *encoder) {
+	// size is the size of the data, not including the strings.
+	const size = 41
+
+	e.Encode(uint16(size + len(s.Name) + len(s.UID) + len(s.GID) + len(s.MUID)))
+	e.Encode(s.Type)
+	e.Encode(s.Dev)
+	e.Encode(s.QIDType)
+	e.Encode(s.Version)
+	e.Encode(s.Path)
+	e.Encode(s.Mode)
+	e.Encode(s.ATime)
+	e.Encode(s.MTime)
+	e.Encode(s.Length)
+	e.Encode(s.Name)
+	e.Encode(s.UID)
+	e.Encode(s.GID)
+	e.Encode(s.MUID)
+}
+
+type Tversion struct {
+	Version string
+}
+
+func (msg Tversion) Type() MessageType {
+	return TversionType
+}
+
+type Rversion struct {
+	Version string
+}
+
+func (msg Rversion) Type() MessageType {
+	return RversionType
+}
+
+type Tauth struct {
+	AFID  uint32
+	Uname string
+	Aname string
+}
+
+func (msg Tauth) Type() MessageType {
+	return TauthType
+}
+
+type Rauth struct {
+	AQID QID
+}
+
+func (msg Rauth) Type() MessageType {
+	return RauthType
+}
+
+type Tattach struct {
+	FID   uint32
+	AFID  uint32
+	Uname string
+	Aname string
+}
+
+func (msg Tattach) Type() MessageType {
+	return TattachType
+}
+
+type Rattach struct {
+	QID QID
+}
+
+func (msg Rattach) Type() MessageType {
+	return RattachType
+}
+
+type Rerror struct {
+	Ename string
+}
+
+func (msg Rerror) Type() MessageType {
+	return RerrorType
+}
+
+type Tflush struct {
+	OldTag uint16
+}
+
+func (msg Tflush) Type() MessageType {
+	return TflushType
+}
+
+type Rflush struct {
+}
+
+func (msg Rflush) Type() MessageType {
+	return RflushType
+}
+
+type Twalk struct {
+	FID    uint32
+	NewFID uint32
+	Wname  []string
+}
+
+func (msg Twalk) Type() MessageType {
+	return TwalkType
+}
+
+type Rwalk struct {
+	WQID []QID
+}
+
+func (msg Rwalk) Type() MessageType {
+	return RwalkType
+}
+
+type Topen struct {
+	FID  uint32
+	Mode uint8 // TODO: Make a Mode type?
+}
+
+func (msg Topen) Type() MessageType {
+	return TopenType
+}
+
+type Ropen struct {
+	QID    QID
+	IOUnit uint32
+}
+
+func (msg Ropen) Type() MessageType {
+	return RopenType
+}
+
+type Tcreate struct {
+	FID  uint32
+	Name string
+	Perm uint32 // TODO: Make a Perm type?
+	Mode uint8  // TODO: Make a Mode type?
+}
+
+func (msg Tcreate) Type() MessageType {
+	return TcreateType
+}
+
+type Rcreate struct {
+	QID    QID
+	IOUnit uint32
+}
+
+func (msg Rcreate) Type() MessageType {
+	return RcreateType
+}
+
+type Tread struct {
+	FID    uint32
+	Offset uint64
+	Count  uint32
+}
+
+func (msg Tread) Type() MessageType {
+	return TreadType
+}
+
+// TODO: Figure out a clean way to allow handlers to send responses
+// via an io.Writer?
+type Rread struct {
+	Data []byte
+}
+
+func (msg Rread) Type() MessageType {
+	return RreadType
+}
+
+// TODO: Figure out a clean way to allow clients request writes via an
+// io.Writer?
+type Twrite struct {
+	FID    uint32
+	Offset uint64
+	Data   []byte
+}
+
+func (msg Twrite) Type() MessageType {
+	return TwriteType
+}
+
+type Rwrite struct {
+	Count uint32
+}
+
+func (msg Rwrite) Type() MessageType {
+	return RwriteType
+}
+
+type Tclunk struct {
+	FID uint32
+}
+
+func (msg Tclunk) Type() MessageType {
+	return TclunkType
+}
+
+type Rclunk struct {
+}
+
+func (msg Rclunk) Type() MessageType {
+	return RclunkType
+}
+
+type Tremove struct {
+	FID uint32
+}
+
+func (msg Tremove) Type() MessageType {
+	return TremoveType
+}
+
+type Rremove struct {
+}
+
+func (msg Rremove) Type() MessageType {
+	return RremoveType
+}
+
+type Tstat struct {
+	FID uint32
+}
+
+func (msg Tstat) Type() MessageType {
+	return TstatType
+}
+
+type Rstat struct {
+	Stat []Stat
+}
+
+func (msg Rstat) Type() MessageType {
+	return RstatType
+}
+
+type Twstat struct {
+	FID  uint32
+	Stat []Stat
+}
+
+func (msg Twstat) Type() MessageType {
+	return TwstatType
+}
+
+type Rwstat struct {
+}
+
+func (msg Rwstat) Type() MessageType {
+	return RwstatType
+}
