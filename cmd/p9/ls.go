c812a410636d21af01e38f69c2b804e0ca0ab9eb
DeedleFake <deedlefake@users.noreply.github.com>
2019-07-14 20:22:26 -0400

    cmd/p9: Better ls.

--- cmd/p9/ls.go
+++ cmd/p9/ls.go
@@ -8,42 +8,61 @@ import (
 	"github.com/DeedleFake/p9"
 )
 
-type lsCmd struct{}
+type lsCmd struct {
+	showDetails bool
+}
 
-func (lsCmd) Name() string {
+func (cmd *lsCmd) Name() string {
 	return "ls"
 }
 
-func (lsCmd) Desc() string {
+func (cmd *lsCmd) Desc() string {
 	return "List the files in a directory."
 }
 
-func (lsCmd) Run(options GlobalOptions, args []string) error {
+func (cmd *lsCmd) Run(options GlobalOptions, args []string) error {
 	fset := flag.NewFlagSet("ls", flag.ExitOnError)
-	showDetails := fset.Bool("l", false, "Show details.")
+	fset.BoolVar(&cmd.showDetails, "l", false, "Show details.")
 	fset.Parse(args[1:])
 
 	return attach(options, func(a *p9.Remote) error {
 		d, err := a.Open(fset.Arg(0), p9.OREAD)
 		if err != nil {
-			return fmt.Errorf("Failed to open %q: %v\n", fset.Arg(0), err)
+			return fmt.Errorf("Failed to open %q: %v", fset.Arg(0), err)
 		}
 		defer d.Close()
 
+		fi, err := d.Stat("")
+		if err != nil {
+			return fmt.Errorf("Failed to stat: %v", err)
+		}
+
+		if fi.Type&p9.QTDir == 0 {
+			cmd.printEntry(fi)
+			return nil
+		}
+
+		fi.Name = "."
+		cmd.printEntry(fi)
+
 		entries, err := d.Readdir()
 		if err != nil {
-			return fmt.Errorf("Failed to read dir: %v\n", err)
+			return fmt.Errorf("Failed to read dir: %v", err)
 		}
 
 		for _, entry := range entries {
-			if *showDetails {
-				fmt.Printf("%v ", os.FileMode(entry.Mode))
-			}
-			fmt.Printf("%v\n", entry.Name)
+			cmd.printEntry(entry)
 		}
 
 		return nil
 	})
+}
+
+func (cmd *lsCmd) printEntry(entry p9.DirEntry) {
+	if cmd.showDetails {
+		fmt.Printf("%v ", os.FileMode(entry.Mode))
+	}
+	fmt.Printf("%v\n", entry.Name)
 }
 
 func init() {
